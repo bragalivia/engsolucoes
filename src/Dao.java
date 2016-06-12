fa1b8f69bd725ed9be65e8411f79904fe594d228
bragalivia <cbraga.livia@gmail.com>
2016-06-12 15:46:06 -0300

    Infrastructure web application

---
+++ src/Dao.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by Livia on 12/06/2016.
+ */
+public class Dao {
+    private static String driver = "com.mysql.jdbc.Driver";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection("jdbc:mysql://localhost/engsolucoes","root", "root");
+    }
+}
